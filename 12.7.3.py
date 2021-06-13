19ee1cc781b2391ed1e9b466edb733485154d580
artkabanov <kabanova-83@mail.ru>
2021-06-13 22:27:36 +0300

    Update 12.7.3.py

--- 12.7.3.py
+++ 12.7.3.py
@@ -5,20 +5,21 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("Введите сумму вклада в рублях: "))
 
 # преобразуем ключи и значения словаря в списки
-banks = list(per_cent.keys())
+bank = list(per_cent.keys())
 deposit = list(per_cent.values())
 
 # пока i меньше длины словаря, заменяем в списке deposit процентную ставку на годовую прибыль от суммы вклада
 i = 0
 while i < len(per_cent):
     deposit[i] = int(deposit[i]*money/100)
-    i = i + 1
+    print("Прибыль в банке", bank[i],"за год составит", deposit[i], "руб.")
+    i += 1
 
 # определяем порядковый номер максимального значения и соответствующий банк
 counts = deposit.index(max(deposit))
-best_bank = banks[counts]
+best_bank = bank[counts]
 max_deposit = deposit[counts]
 
 # вывод годовой прибыли по всем банкам, максимальное значение и банк
 print(deposit)
-print("Максимальная сумма, которую вы можете заработать:", max_deposit, "руб. в банке", best_bank)
+print("Максимальная сумма, которую вы можете заработать -", max_deposit, "руб. в банке", best_bank)
